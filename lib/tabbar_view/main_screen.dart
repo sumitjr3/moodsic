31b5152977b5bc18a210733b1984be721837e655
sumitjr3 <103193051+sumitjr3@users.noreply.github.com>
2024-02-21 13:46:28 +0530

    themes integrated

--- lib/tabbar_view/main_screen.dart
+++ lib/tabbar_view/main_screen.dart
@@ -2,19 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:moodsic/tabbar_view/tabs/nature_sound_view.dart';
 import 'package:moodsic/tabbar_view/tabs/profile.dart';
 import 'package:moodsic/tabbar_view/tabs/tunes_view.dart';
+import 'package:moodsic/theme/app_theme.dart';
+import 'package:moodsic/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const DefaultTabController(
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    Color? tabbarcolor;
+    if (themeProvider.selectedTheme == AppTheme.studyTheme()) {
+      tabbarcolor = AppTheme.studyTheme().colorScheme.primary;
+    } else if (themeProvider.selectedTheme == AppTheme.taskTheme()) {
+      tabbarcolor = AppTheme.taskTheme().colorScheme.primary;
+    } else if (themeProvider.selectedTheme == AppTheme.readTheme()) {
+      tabbarcolor = AppTheme.readTheme().colorScheme.primary;
+    } else if (themeProvider.selectedTheme == AppTheme.chillTheme()) {
+      tabbarcolor = AppTheme.chillTheme().colorScheme.primary;
+    } else {
+      tabbarcolor = AppTheme.focusTheme().colorScheme.primary;
+    }
+
+    return DefaultTabController(
       length: 3,
       child: Scaffold(
         body: Column(
           children: [
             TabBar(
               indicatorSize: TabBarIndicatorSize.label,
+              labelColor: tabbarcolor,
+              unselectedLabelColor: tabbarcolor,
               tabs: [
                 Tab(
                   iconMargin: EdgeInsets.only(bottom: 3),
@@ -40,7 +60,6 @@ class MainScreen extends StatelessWidget {
                   ),
                 ),
               ],
-              unselectedLabelColor: Colors.grey,
             ),
             Expanded(
               child: TabBarView(
--- lib/tabbar_view/tabs/tunes_view.dart
+++ lib/tabbar_view/tabs/tunes_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:moodsic/theme/app_theme.dart';
 import 'package:moodsic/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:moodsic/theme/app_theme.dart';
 
 class tunesView extends StatelessWidget {
   const tunesView({super.key});
@@ -12,20 +13,27 @@ class tunesView extends StatelessWidget {
     final themeProvider = Provider.of<ThemeProvider>(context);
 
     String selectedThemeText;
+    Color backgroundColor;
 
     // Compare the selected theme with each theme individually
     if (themeProvider.selectedTheme == AppTheme.studyTheme()) {
       selectedThemeText = 'Study Theme';
+      backgroundColor = AppTheme.studyTheme().colorScheme.background;
     } else if (themeProvider.selectedTheme == AppTheme.taskTheme()) {
       selectedThemeText = 'Task Theme';
+      backgroundColor = AppTheme.taskTheme().colorScheme.background;
     } else if (themeProvider.selectedTheme == AppTheme.readTheme()) {
       selectedThemeText = 'Read Theme';
+      backgroundColor = AppTheme.readTheme().colorScheme.background;
     } else if (themeProvider.selectedTheme == AppTheme.chillTheme()) {
       selectedThemeText = 'Chill Theme';
+      backgroundColor = AppTheme.chillTheme().colorScheme.background;
     } else {
       selectedThemeText = 'Focus Theme';
+      backgroundColor = AppTheme.focusTheme().colorScheme.background;
     }
     return Scaffold(
+      backgroundColor: backgroundColor,
       body: Center(
         child: Text(selectedThemeText),
       ),

83f36fb4caf32694be110fb4823fdd4c590be8cb
sumitjr3 <103193051+sumitjr3@users.noreply.github.com>
2024-02-25 17:53:20 +0530

    deatiled activity ui updated

--- lib/views/detailed_music_player.dart
+++ lib/views/detailed_music_player.dart
@@ -16,77 +16,136 @@ class DetailedMusicPlayer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: const Icon(Icons.arrow_back),
-        ),
-      ),
-      body: Center(
-        child: Card(
-          shadowColor: Colors.deepPurple[400],
-          margin: const EdgeInsets.all(20),
-          elevation: 20,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30),
-          ),
+      body: SafeArea(
+        child: Center(
           child: Column(
             children: [
-              Container(
-                padding: const EdgeInsets.all(20),
-                alignment: AlignmentDirectional.center,
-                child: Image(
-                  image: AssetImage(imagepath),
-                  alignment: Alignment.center,
-                  fit: BoxFit.cover,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back,
+                      size: 25,
+                    ),
+                  ),
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                      letterSpacing: 1,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(
+                      Icons.menu,
+                      size: 30,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Card(
+                shadowColor: Colors.deepPurple[400],
+                margin: const EdgeInsets.all(20),
+                elevation: 20,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
                 ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Text(
-                text,
-                style: const TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-              Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                child: Column(
                   children: [
-                    IconButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        icon: const Icon(Icons.arrow_left_rounded),
-                        iconSize: 50),
-                    const SizedBox(
-                      width: 10,
+                    Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(30),
+                        child: Image(
+                          image: AssetImage(imagepath),
+                          alignment: Alignment.center,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
                     ),
-                    IconButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      icon: const Icon(Icons.pause_circle),
-                      iconSize: 50,
+                    const SizedBox(height: 10),
+                    Text(
+                      text,
+                      style: const TextStyle(
+                        fontSize: 20,
+                      ),
                     ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      icon: const Icon(Icons.arrow_right_rounded),
-                      iconSize: 50,
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
+                    const SizedBox(height: 10),
                   ],
+                ),
+              ),
+              SliderTheme(
+                data: SliderTheme.of(context).copyWith(
+                  thumbShape:
+                      const RoundSliderThumbShape(enabledThumbRadius: 0),
+                ),
+                child: Slider(
+                  min: 0,
+                  max: 100,
+                  value: 50,
+                  activeColor: Colors.green,
+                  onChanged: (value) {},
+                ),
+              ),
+              Card(
+                shadowColor: Colors.deepPurple[400],
+                margin: const EdgeInsets.all(20),
+                elevation: 20,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Icon(
+                            Icons.skip_previous,
+                            size: 25,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      Expanded(
+                        flex: 2,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Icon(
+                            Icons.play_arrow,
+                            size: 25,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Icon(
+                            Icons.skip_next,
+                            size: 25,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
